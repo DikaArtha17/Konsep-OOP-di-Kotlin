5dfecb51948870395dc7d50b79884608e1c3259d
MSI GAMING <arthdika17@gmail.com>
2024-09-23 23:27:29 +0700

    add

---
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/DataClassC.kt
@@ -0,0 +1,13 @@
+package id.ac.polbeng.depandi.test_kelas
+
+fun main(args: Array<String>) {
+
+    val meri = Student("Meri", 20)
+    val badu = meri.copy("Badu")
+    if(meri.equals(badu))
+        println("meri is equal to badu.")
+    else
+        println("meri is not equal to badu.")
+    println("Hashcode of meri: ${meri.hashCode()}")
+    println("Hashcode of badu: ${badu.hashCode()}")
+}
